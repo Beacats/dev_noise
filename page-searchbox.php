c70221ae65dc9417175c9a5149694cf5ef2d37dd
Beacats <dev@beacats.com>
2023-01-19 23:27:00 +0900

    WIP

--- page-searchbox.php
+++ page-searchbox.php
@@ -18,13 +18,12 @@ $args = array(
     'post_type' => 'emc', //カスタム投稿slag
     'orderby' => 'date', //日付を出力する基準
     'order' => 'DESC' //表示する順番（逆はASC）
-
 );
 $search_products = get_posts($args);
 if ($search_products) : foreach ($search_products as $post) : setup_postdata($post); // 繰り返し START
 ?>
 
-        <!-- ここにループさせたい内容を書く -->
+
 <?php
 
     $class_array = array(); // class用の配列を定義
@@ -34,47 +33,39 @@ if ($search_products) : foreach ($search_products as $post) : setup_postdata($po
 
     if ($emc_tag_raw) : // 検索用製品カテゴリが存在していれば$class_arrayに追加
         foreach ($emc_tag_raw as $emc_tag) {
-            $emc_tag_name = 'msd'. $emc_tag->name;
+            $emc_tag_name = $emc_tag->slug . ' ';
             array_push($class_array, $emc_tag_name);
         }
     endif;
     if ($emc_industry_raw) : // 検索用業界・産業が存在していれば$class_arrayに追加
         foreach ($emc_industry_raw as $emc_industry) {
-            $emc_industry_name = 'msd'. $emc_industry->name;
+            $emc_industry_name = $emc_industry->slug . ' ';
             array_push($class_array, $emc_industry_name);
         }
     endif;
     if ($emc_test_standard_tag_raw) : // 検索用試験規格が存在していれば$class_arrayに追加
         foreach ($emc_test_standard_tag_raw as $emc_test_standard_tag) {
-            $emc_test_standard_tag_name = 'msd'. $emc_test_standard_tag->name;
+            $emc_test_standard_tag_name = $emc_test_standard_tag->slug . ' ';
             array_push($class_array, $emc_test_standard_tag_name);
         }
     endif;
-    
-    
-    
-    if ($emc_tag_raw || $emc_industry_raw || $emc_test_standard_tag_raw) : // 検索用製品カテゴリ,検索用業界・産業,検索用試験規格のいずれかを持つか判定
+
+    if ($class_array) : // 検索用製品カテゴリ,検索用業界・産業,検索用試験規格のいずれかを持つか判定
         // 検索用タグを持つ場合
-        echo the_field('product_title_main');
         ?>
-        <li class='<?php
-        foreach ($class_array as $kono_class):
-        echo $kono_class;
-        ?> <?php
-        endforeach;
-        ?>'>
+        <li class="<?php foreach ($class_array as $this_class) : echo $this_class; endforeach;?>">
+        <?php echo the_field('product_title_main'); ?>
+        </li>
         <?php
     else:
         // 検索用タグを持たない場合
         echo '検索タグがありません';
     endif;
-// foreach( $terms as $term ) {
-// echo '<li>'.$term->name.'</li>';
-// }
+
 ?>
 
 
- 
+
     <?php endforeach; // 繰り返し END
     ?>
 </ul>
